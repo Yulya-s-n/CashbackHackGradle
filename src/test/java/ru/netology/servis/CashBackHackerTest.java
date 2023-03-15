7ad05f7a97d58955c802a1b9ae6553beea1af316
yuliya <yulya.s.n@bk.ru>
2023-03-15 13:50:23 +0300

    new test

--- src/test/java/ru/netology/servis/CashBackHackerTest.java
+++ src/test/java/ru/netology/servis/CashBackHackerTest.java
@@ -9,32 +9,35 @@ public class CashBackHackerTest {
     CashBackHacker service = new CashBackHacker();
 
     @Test
-    public void testBorder() {
-        //1.подгтовка данных2.выполнение целевого действия3.сравние факт и ожид результ.
-        //создаем сервис и объект куда будем передавать данные
+    public void shouldCountRemain() {
+
 
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
+
         assertEquals(actual, expected);
     }
 
     @Test
-    public void amountAfterWound() {
+    public void shouldCountRemainIfZero() {
+
 
         int actual = service.remain(0);
         int expected = 1000;
+
         assertEquals(actual, expected);
     }
 
     @Test
-    public void amountBeforeBorder() {
-
-        int actual = service.remain(2999);
+    public void shouldCountBorderIfLess() {
+        int actual = service.remain(1999);
         int expected = 1;
+
         assertEquals(actual, expected);
     }
+
     @Test
-    public void amountBorderIfMore() {
+    public void shouldCountBorderIfMore() {
 
 
         int actual = service.remain(2001);
