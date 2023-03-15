3003b51f3f93b89389a9922f43139e0346099b23
yuliya <yulya.s.n@bk.ru>
2023-03-15 13:49:31 +0300

    new test

--- src/test/java/ru/netology/servis/CashBackHackerTest.java
+++ src/test/java/ru/netology/servis/CashBackHackerTest.java
@@ -2,44 +2,48 @@ package ru.netology.servis;
 
 
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashBackHackerTest {
+    CashBackHacker service = new CashBackHacker();
 
 
-        CashBackHacker service = new CashBackHacker();
+    @Test
+    public void shouldCountRemain() {
 
-        @Test
-        public void testBorder() {
-            //1.подгтовка данных2.выполнение целевого действия3.сравние факт и ожид результ.
-            //создаем сервис и объект куда будем передавать данные
 
-            int actual = service.remain(0);
-            int expected = 1000;
-            assertEquals(actual, expected);
-        }
+        int actual = service.remain(1000);
+        int expected = 0;
 
-        @Test
-        public void amountAfterWound() {
-
-            int actual = service.remain(1);
-            int expected = 999;
-            assertEquals(actual, expected);
-        }
-
-        @Test
-        public void amountBeforeBorder() {
-
-            int actual = service.remain(999);
-            int expected = 1;
-            assertEquals(actual, expected);
-        }
-        @Test
-        public void amountBorderIfMore() {
-
-            int actual = service.remain(300);
-            int expected = 700;
-
-            assertEquals(actual, expected);
-        }
+        assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldCountRemainIfZero() {
+
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountBorderIfLess() {
+        int actual = service.remain(1999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountBorderIfMore() {
+
+
+        int actual = service.remain(2001);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+}
