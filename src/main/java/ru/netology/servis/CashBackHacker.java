05cd410f91c5b1e0fbd22fc1fc3a07252bff6ef9
yuliya <yulya.s.n@bk.ru>
2023-03-15 12:03:10 +0300

    replacement assert

--- src/main/java/ru/netology/servis/CashBackHacker.java
+++ src/main/java/ru/netology/servis/CashBackHacker.java
@@ -3,6 +3,7 @@ package ru.netology.servis;
 public class CashBackHacker {
     private final int boundary = 1000;
 
+
     public int remain(int amount) {
         return boundary - amount % boundary;
     }
