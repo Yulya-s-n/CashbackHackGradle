bc70b38fce2bd9a57d061492b2bb162585d40271
yuliya <yulya.s.n@bk.ru>
2023-03-15 12:57:12 +0300

    replacement assert

--- src/test/java/ru/netology/servis/CashBackHackerTest.java
+++ src/test/java/ru/netology/servis/CashBackHackerTest.java
@@ -1,4 +1,45 @@
-import static org.junit.Assert.*;
+package ru.netology.servis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class CashBackHackerTest {
-  
+    CashBackHacker service = new CashBackHacker();
+
+    @Test
+    public void testBorder() {
+        //1.подгтовка данных2.выполнение целевого действия3.сравние факт и ожид результ.
+        //создаем сервис и объект куда будем передавать данные
+
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountAfterWound() {
+
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountBeforeBorder() {
+
+        int actual = service.remain(2999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void amountBorderIfMore() {
+
+
+        int actual = service.remain(2001);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
 }
